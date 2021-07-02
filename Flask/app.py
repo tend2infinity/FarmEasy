e05e5c95ca3b3555ab83e267553024fc25801acc
Thari zephaniah <56599367+Zeph-T@users.noreply.github.com>
2021-07-02 12:05:30 +0530

    basic folder structure

---
+++ Flask/app.py
@@ -0,0 +1,11 @@
+from flask import Flask,request
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return {"Status":"Connected!"}
+
+if __name__== '__main__':
+    app.run(debug = True)
+    
